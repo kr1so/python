79b4e678b98c68810fe1dbde5144c68939480b0d
Kristina <yajkakusta@gmail.com>
2022-01-24 20:54:33 +0500

    Управляющая конструкция elif

---
+++ enter_to_python/example11.py
@@ -0,0 +1,13 @@
+# Продолжение управляющих конструкций
+
+#elif
+
+username = input('Введите Ваше имя\n')
+if username == 'Маша':
+    print('Ура! Это же Маша!')
+elif username == 'Марина':
+    print('Я так ждала, Вас, Марина')
+elif username == ' Ильнар':
+    print('Ильнар - топчик')
+else:
+    print('Привет,', username)
